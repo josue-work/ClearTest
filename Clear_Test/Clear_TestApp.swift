686021798f43ab1559d2707f821bd106d853657e
Josue M Cizungu <josue@parentsense.app>
2024-05-12 12:59:37 +0200

    Initial Commit

---
+++ Clear_Test/Clear_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clear_TestApp.swift
+//  Clear_Test
+//
+//  Created by Josue Muhiri Cizungu on 2024/05/12.
+//
+
+import SwiftUI
+
+@main
+struct Clear_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clear_Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clear_Test
+//
+//  Created by Josue Muhiri Cizungu on 2024/05/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
